a07d7b703b1a05cc7ca22b7f9e3d9179167f7fa7
UshanKaushalya <Ushanwk22@gmail.com>
2023-11-25 12:06:09 +0530

    User done

--- User-Service/user-server/src/main/java/com/example/userserver/UserServerApplication.java
+++ User-Service/user-server/src/main/java/com/example/userserver/UserServerApplication.java
@@ -3,9 +3,11 @@ package com.example.userserver;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServerApplication {
 
     public static void main(String[] args) {
