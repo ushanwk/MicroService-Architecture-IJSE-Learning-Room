114afad5c0f655b319927bee8dda15ea90b47ceb
UshanKaushalya <Ushanwk22@gmail.com>
2023-11-25 12:56:29 +0530

    registry done

---
+++ Service-Registry/server-registry/src/main/java/com/example/serverregistry/ServerRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.example.serverregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerRegistryApplication.class, args);
+	}
+
+}
