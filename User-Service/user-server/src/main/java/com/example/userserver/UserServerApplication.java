d3fc5708dee00dc62df46b99f001ee8ad7fdb739
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-09 12:12:11 +0530

    done

--- User-Service/user-server/src/main/java/com/example/userserver/UserServerApplication.java
+++ User-Service/user-server/src/main/java/com/example/userserver/UserServerApplication.java
@@ -3,9 +3,12 @@ package com.example.userserver;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
